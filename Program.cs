c1f48d594c287430309b90a4353998f8de9ad71c
PNZeml <pavelz1n@yandex.ru>
2016-11-13 12:39:27 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Xml;
-using System.IO;
-using System.Collections.Generic;
-    
-using SharpCompress;
-using SharpCompress.Archive.Rar;
-using SharpCompress.Reader;
 using SharpCompress.Common;
+using SharpCompress.Reader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
 
 namespace ProjectForKSONW_XMLParse 
 {
@@ -19,19 +16,16 @@ namespace ProjectForKSONW_XMLParse
         public byte cateSubMCP;     //КатСубМСП
         public byte specNewMCP;     //ПризНовМСП
     }
-
     class Program
     {
         static string projDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
-        static void Main(string[] args)
+        static void filesProcess(List<Doc> docs, string[] fileEntries)
         {
-            List<Doc> docs = new List<Doc>();
-            string s = projDir + @"\xmlExamples";
-            string[] fileEntries = Directory.GetFiles(s);
+            string s = projDir + @"\temp";
             foreach (string fileName in fileEntries)
             {
                 XmlDocument xd = new XmlDocument();
-                System.Console.WriteLine(fileName);
+                //System.Console.WriteLine(fileName);
                 s = fileName;
                 xd.Load(s);
                 XmlNodeList nodes = xd.DocumentElement.SelectNodes("/Файл/Документ");
@@ -45,30 +39,37 @@ namespace ProjectForKSONW_XMLParse
                     docs.Add(doc);
                 }
             }
-            /*foreach (Doc doc in docs)
-            {
-                Console.WriteLine(doc.idDoc);
-                Console.WriteLine("==ДатаСост:" + doc.dateState);
-                Console.WriteLine("==ДатаВклМСП:" + doc.dateInclude);
-            }*/
-            Console.WriteLine(docs.Count);
+        }
+        static void Main(string[] args)
+        {
+            List<Doc> docs = new List<Doc>();
+            string[] fileEntries;
+            string filePath = projDir + @"\xmlExamples.rar";
 
-            string s1 = projDir + @"\data-10102016-structure-08012016.zip";
-            int i = 0;
-            using (Stream stream = File.OpenRead(s1))
-            {
+            //using Stream begin
+            using (Stream stream = File.OpenRead(filePath)) {
                 var reader = ReaderFactory.Open(stream);
-                while (reader.MoveToNextEntry())
-                {
-                    if (!reader.Entry.IsDirectory)
-                    {
-                        //Console.WriteLine(reader.Entry.FilePath);
-                        i++;
-                        //reader.WriteEntryToDirectory(@"C:\temp", ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                while (reader.MoveToNextEntry()) {
+                    if (!reader.Entry.IsDirectory) {
+                        reader.WriteEntryToDirectory(projDir + @"\temp", ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+                        fileEntries = Directory.GetFiles(projDir + @"\temp");
+                        if (fileEntries.Length >= 10) {
+                            filesProcess(docs, fileEntries);
+                            Array.ForEach(Directory.GetFiles(projDir + @"\temp"), File.Delete);
+                        }
+                    } else {
+                        fileEntries = Directory.GetFiles(projDir + @"\temp");
+                        if (fileEntries.Length > 0) {
+                            filesProcess(docs, fileEntries);
+                            Array.ForEach(Directory.GetFiles(projDir + @"\temp"), File.Delete);
+                        }
                     }
                 }
             }
-            Console.WriteLine(i);
+            //using Stream end
+
+            Console.WriteLine(docs.Count);
         }
+        //Main End
     }
 }
