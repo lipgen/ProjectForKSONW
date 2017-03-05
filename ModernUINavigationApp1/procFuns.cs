36294b0b17f655b0810deea5f27cf7591aef564e
PNZeml <pavelz1n@yandex.ru>
2017-03-05 13:16:51 +0700

    final version of files

--- ModernUINavigationApp1/procFuns.cs
+++ ModernUINavigationApp1/procFuns.cs
@@ -19,35 +19,36 @@ namespace ModernUINavigationApp1
     public class Document
     {
         //таб "документ"
-        public string a1;//ИдДок
-        public string a2;//ДатаСост                     
-        public string a3;//ДатаВклМСП                   
-        public string a4;//ВидСубМСП                    
-        public string a5;//КатСубМСП
-        public string a6;//ПризНовМСП                   
+        public string a1 { get; set; }//ИдДок
+        public string a2 { get; set; }//ДатаСост                     
+        public string a3 { get; set; }//ДатаВклМСП                   
+        public string a4 { get; set; }//ВидСубМСП                    
+        public string a5 { get; set; }//КатСубМСП
+        public string a6 { get; set; }//ПризНовМСП                   
         //таб "сведосн"
-        public string b1;//ИННФЛ или ИННЮЛ              
-        public string b2;//ЮЛ_НаимОрг или ИП_Фамилия   
-        public string b3;//ЮЛ_НаимОргСокр или ИП_Имя                   
-        public string b4;//ИП_Отчество                 
+        public string b1 { get; set; }//ИННФЛ или ИННЮЛ              
+        public string b2 { get; set; }//ЮЛ_НаимОрг или ИП_Фамилия   
+        public string b3 { get; set; }//ЮЛ_НаимОргСокр или ИП_Имя                   
+        public string b4 { get; set; }//ИП_Отчество                 
         //таб "сведмн"
-        public string c1;//КодРегион                    
-        public string c2;//Регион                      
-        public string c3;//Район  
-        public string c4;//Город
-        public string c5;//НаселПункт 
+        public string c1 { get; set; }//КодРегион                    
+        public string c2 { get; set; }//Регион                      
+        public string c3 { get; set; }//Район  
+        public string c4 { get; set; }//Город
+        public string c5 { get; set; }//НаселПункт 
         //таб "conn_документ-своквэд"
         public InfoRCoEA InfoRCoEAmain;//ОснОКВЭД
         public InfoRCoEA[] InfoRCoEAadd;//ДопСвОКВЭД
+        public List<InfoRCoEA> lInfoRCoEA;
         //таб "conn_документ-свлиценз"
         public InfoLic[] InfoLics;
     }
     //Класс для хранения копии данных СвОКВЭД - "Russian Classification of Economic Activities"
     public class InfoRCoEA
     {
-        public string a1;//КодОКВЭД
-        public string a2;//НаимОКВЭД
-        public string a3;//ВерсОКВЭД
+        public string a1 { get; set; }//КодОКВЭД
+        public string a2 { get; set; }//НаимОКВЭД
+        public string a3 { get; set; }//ВерсОКВЭД
     }
     //Класс для хранения копии данных СвЛиценз
     public class InfoLic
@@ -65,16 +66,29 @@ namespace ModernUINavigationApp1
 
     class procFuns
     {
-        public string pd = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        //public string pd = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+        public string pd = Directory.GetCurrentDirectory();
         public void settingParser(string[] sCol)
         {
             string[] data;
             if (File.Exists(pd + @"\setting.ini")) {
                 data = File.ReadAllLines(pd + @"\setting.ini");
             } else {
-                data = new string[] { "[db_setting]", "server=localhost", "user=root", "database=reg19", "port=3306", "password=1234",
-                                      "[data_getting_setting]", "rsmpURL=https://www.nalog.ru/opendata/7707329152-rsmp", "lastArchDate=01.01.2016",
-                                      "lastArchURL=", "deletePriv=1" };
+                data = new string[] { "[db_setting]",
+                                      "server=localhost",                                            //sCol[0]
+                                      "user=root",                                                   //sCol[1]
+                                      "database=reg19",                                              //sCol[2]
+                                      "port=3306",                                                   //sCol[3]
+                                      "password=1234",                                               //sCol[4]
+                                      "[data_getting_setting]",
+                                      "rsmpURL=https://www.nalog.ru/opendata/7707329152-rsmp",
+                                      "lastArchDate=01.01.2016",
+                                      "lastArchURL=",
+                                      "deletePriv=1",
+                                      "[excel_setting]",
+                                      "officeVersion =",
+                                      "savePath =};"
+                };
                 File.WriteAllLines(pd + @"\setting.ini", data);
             }
             int j = 0;
@@ -102,49 +116,5 @@ namespace ModernUINavigationApp1
             cmd.ExecuteNonQuery();
         }
 
-        public void CreateSheet()
-        {
-            try
-            {
-
-
-                Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
-
-                if (xlApp == null)
-                {
-                    MessageBox.Show("Excel is not properly installed!!");
-                    return;
-                }
-
-
-                Excel.Workbook xlWorkBook;
-                Excel.Worksheet xlWorkSheet;
-                object misValue = System.Reflection.Missing.Value;
-
-                xlWorkBook = xlApp.Workbooks.Add(misValue);
-                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
-
-                xlWorkSheet.Cells[1, 1] = "ID";
-                xlWorkSheet.Cells[1, 2] = "Name";
-                xlWorkSheet.Cells[2, 1] = "1";
-                xlWorkSheet.Cells[2, 2] = "One";
-                xlWorkSheet.Cells[3, 1] = "2";
-                xlWorkSheet.Cells[3, 2] = "Two";
-
-                xlWorkBook.SaveAs(@"c:\tmp\csharp-Excel.xls", Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
-                xlWorkBook.Close(true, misValue, misValue);
-                xlApp.Quit();
-
-                Marshal.ReleaseComObject(xlWorkSheet);
-                Marshal.ReleaseComObject(xlWorkBook);
-                Marshal.ReleaseComObject(xlApp);
-
-                MessageBox.Show("Excel file created , you can find the file c:\\csharp-Excel.xls");
-            }
-            catch (Exception ex)
-            {
-
-            }
-        }
     }
 }
